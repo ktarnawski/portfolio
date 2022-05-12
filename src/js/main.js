d449aa28233472989831196e92c28cf8c4de56f1
Krzysztof Tarnawski <k.tarnawski1@gmail.com>
2022-05-12 02:16:18 +0200

    Add theme and color selector

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,44 @@
 const burgerBtn = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
 const allNavLi = document.querySelectorAll(".nav__links-link");
+const colorButton = document.querySelector(".style-selector__color-button");
+const themeButton = document.querySelector(".style-selector__theme-button");
+const styleSelector = document.querySelector(".style-selector");
+const allStyles = document.querySelectorAll(".alt-style");
+
+const handleDarkLightIcon = () => {
+	themeButton.querySelector("i").classList.toggle("fa-sun");
+	themeButton.querySelector("i").classList.toggle("fa-moon");
+	document.body.classList.toggle("dark");
+};
+
+const handleDarkLightIconOnLoad = () => {
+	if (document.body.classList.contains("dark")) {
+		themeButton.querySelector("i").classList.add("fa-sun");
+	} else {
+		themeButton.querySelector("i").classList.add("fa-moon");
+	}
+};
+
+const handleStyleSelector = () => {
+	styleSelector.classList.toggle("open");
+};
+
+const hideStyleSelectorOnScroll = () => {
+	if (styleSelector.classList.contains("open")) {
+		styleSelector.classList.remove("open");
+	}
+};
+
+const setActiveStyle = (color) => {
+	allStyles.forEach((style) => {
+		if (color === style.getAttribute("title")) {
+			style.removeAttribute("disabled");
+		} else {
+			style.setAttribute("disabled", "true");
+		}
+	});
+};
 
 const closeNav = () => {
 	nav.classList.remove("show-nav");
@@ -26,3 +64,7 @@ $(".progress .progress-in").each(function () {
 
 burgerBtn.addEventListener("click", handleBurger);
 allNavLi.forEach((li) => li.addEventListener("click", closeNav));
+colorButton.addEventListener("click", handleStyleSelector);
+themeButton.addEventListener("click", handleDarkLightIcon);
+window.addEventListener("scroll", hideStyleSelectorOnScroll);
+window.addEventListener("load", handleDarkLightIconOnLoad);
